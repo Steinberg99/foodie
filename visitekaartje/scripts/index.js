27a13503ce498dd2a0074fc3e3a45873d708ab43
Steinberg99 <stein@schaapontwerpers.nl>
2023-02-09 15:19:39 +0100

    Added popup styling

--- visitekaartje/scripts/index.js
+++ visitekaartje/scripts/index.js
@@ -1,23 +1,56 @@
+const biographyContent = document.querySelector(".notes-content");
+
 const id = "cldeozsp33vc90bw53gqzjjti";
 
 const init = async () => {
-  const data = fetchData(id);
+  const data = await fetchData(id);
+
+  console.log(data);
 
   setData(data);
 };
 
+// Fetch user data based on id
+
 const fetchData = async (id) => {
-  const url =
-    "https://cors-anywhere.herokuapp.com/https://whois.fdnd.nl/api/v1/members?first=100";
+  const url = `https://cors-anywhere.herokuapp.com/https://whois.fdnd.nl/api/v1/member?id=${id}`;
   const response = await fetch(url);
   const data = await response.json();
-  const members = data.members;
 
-  return members.filter((member) => member.id === id)[0];
+  return data.member;
 };
 
+// Set the user data
+
 const setData = (data) => {
-  console.log(data);
+  biographyContent.innerHTML = `
+  <img src="${data.avatar}" />
+
+  ${getBiographyDivs(data)}
+  `;
+};
+
+const getBiographyDivs = (data) => {
+  const biographyDivs = [
+    { label: "Name:", value: `${data.name} ${data.surname}` },
+    { label: "Nickname:", value: data.nickname },
+    { label: "Bio:", value: data.bio.html },
+    { label: "GitHub handle:", value: data.gitHubHandle },
+  ]
+    .map((obj) => biographyDiv(obj))
+    .join(" ");
+
+  return biographyDivs;
+};
+
+const biographyDiv = (obj) => {
+  return `
+  <div>
+    <h2>${obj.label}</h2>
+
+    <p>${obj.value}</p>
+  </div>
+  `;
 };
 
 init();
